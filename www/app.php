cd9a3a5869a167a2de9ab6085d11c4545e6bbc2c
Ragnar Mellbin <ragnar.mellbin@neotechnology.com>
2016-11-30 15:29:02 +0100

    Style for main appy

--- www/app.php
+++ www/app.php
@@ -10,14 +10,7 @@ if(empty($_SESSION['user_name'])){
   <title>julkl-app</title>
   <meta charset="utf-8" />
 
-<style type="text/css">
-
-#lists{
-  float:left;
-  text-align: left
-}
-
-</style>
+  <link rel="stylesheet" type="text/css" href="style.css">
 
   <script src="http://code.jquery.com/jquery.min.js"></script>
   <script>
@@ -26,30 +19,32 @@ if(empty($_SESSION['user_name'])){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("main").innerHTML = this.responseText;
+        document.getElementById("main").innerHTML = this.responseText;
       }
     };
     xhttp.open("GET", "editList.php", true);
     xhttp.send();
   }
 
-   $(document).ready(function () {
+  $(document).ready(function () {
 
-   });
-   </script>
+  });
+  </script>
 
 </head>
 <body>
-  <div id = "top">
+  <div class="header">
     <p style="float: left; width: 33.3%; text-align: left"><input type=button value="View your list" onclick="editList()"></p>
     <p style="float: left; width: 33.3%; text-align: center;">julkl-app v0.1</p>
     <p style="float: left; width: 33.3%; text-align: right;"><a href="login.php?action=logout">Log out</a></p>
   </div>
-  <div id = "lists">
-    Lists goes here.
-  </div>
-  <div id = "main">
-    Use the buttons on the page to start using the julkl-app.
+  <div class="flex-container">
+    <nav class="flex-list">
+      Lists goes here.
+    </nav>
+    <div id="main" class="flex-content">
+      Use the buttons on the page to start using the julkl-app.
+    </div>
   </div>
 </body>
 </html>
