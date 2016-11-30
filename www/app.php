8f86f70f4773c1f49afa4e6eb72be8cb4e47cedf
Ragnar Mellbin <ragnar.mellbin@neotechnology.com>
2016-11-30 22:46:02 +0100

    List editing now works

--- www/app.php
+++ www/app.php
@@ -15,26 +15,25 @@ if(empty($_SESSION['user_name'])){
   <script src="http://code.jquery.com/jquery.min.js"></script>
   <script>
 
-  function editList() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("main").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "editList.php", true);
-    xhttp.send();
-  }
-
   $(document).ready(function () {
-
+    $('button#listbutton').on('click', function() {
+      $('#main').load('editList.php');
+    });
   });
+
+  $(document).ajaxComplete(function(){
+    $('button#savelist').on('click', function() {
+      var list = {list_text:$('#mylist').val()};
+      $('#main').load('editList.php', list);
+    });
+  });
+
   </script>
 
 </head>
 <body>
   <div class="header">
-    <p style="float: left; width: 33.3%; text-align: left"><input type=button value="View your list" onclick="editList()"></p>
+    <p style="float: left; width: 33.3%; text-align: left"><button id="listbutton">View your list</button></p>
     <p style="float: left; width: 33.3%; text-align: center;">julkl-app v0.1</p>
     <p style="float: left; width: 33.3%; text-align: right;"><a href="login.php?action=logout">Log out</a></p>
   </div>
--- www/editList.php
+++ www/editList.php
@@ -4,29 +4,48 @@ if(empty($_SESSION['user_name'])){
   header("Location: index.php?feedback=Log%20in%20first.");
   die("Log in first.");
 }
-$sql = 'SELECT list_id, list_text
-FROM lists
-WHERE user_id = :user_id';
-$query = $db->prepare($sql);
-$query->bindValue(':user_id', $_SESSION['user_id']);
-$query->execute();
-$result_row = $query->fetch();
-error_log($result_row);
-if ($result_row) {
-  ?>
-  
-  <?php
-} else {
-  $list_sql = 'INSERT INTO lists (user_id, list_text)
-  VALUES(:user_id, :list_text)';
+if (isset($_POST["list_text"])) {
+  $list_sql = 'UPDATE lists
+  set list_text = :list_text
+  WHERE user_id = :user_id';
   $query = $db->prepare($list_sql);
   $query->bindValue(':user_id', $_SESSION['user_id']);
-  $query->bindValue(':list_text', "");
+  $list_text = htmlentities($_POST['list_text'], ENT_QUOTES);
+  $query->bindValue(':list_text', $list_text);
   $success = $query->execute();
   if( $success ){
-    include("editList.php");
+    echo "List updated.";
   } else {
     echo "Shit hit the fan.";
   }
+} else {
+
+  $sql = 'SELECT list_id, list_text
+  FROM lists
+  WHERE user_id = :user_id';
+  $query = $db->prepare($sql);
+  $query->bindValue(':user_id', $_SESSION['user_id']);
+  $query->execute();
+  $result_row = $query->fetch();
+  if ($result_row) {
+    ?>
+    <textarea id="mylist"><?= $result_row['list_text'] ?></textarea>
+    <button id="savelist"> Save changes</button>
+
+    <?php
+  } else {
+    $list_sql = 'INSERT INTO lists (user_id, list_text)
+    VALUES(:user_id, :list_text)';
+    $query = $db->prepare($list_sql);
+    $query->bindValue(':user_id', $_SESSION['user_id']);
+    $query->bindValue(':list_text', "");
+    $success = $query->execute();
+    if( $success ){
+      include("editList.php");
+    } else {
+      echo "Shit hit the fan.";
+    }
+  }
 }
+
 ?>
